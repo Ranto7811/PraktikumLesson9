37bd460f2416a72f681ba261100e73b18ddb7e04
aviliairs <avilia.indira29@gmail.com>
2023-10-29 12:29:30 +0700

    Menambahkan kode permintaan

--- app/src/main/java/com/example/lesson9/MainActivity.kt
+++ app/src/main/java/com/example/lesson9/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.vitha.roomwordsample.R
 
 class MainActivity : AppCompatActivity() {
+    private val newWordActivityRequestCode = 1
     private val wordViewModel: WordViewModel by viewModels {
         WordViewModelFactory((application as WordsApplication).repository)
     }
