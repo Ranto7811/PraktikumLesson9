93388e28063dc09ea89e91ec737067d3a3e326fc
aviliairs <avilia.indira29@gmail.com>
2023-10-29 12:16:50 +0700

    Mmemperbarui kode di NewWordActivity.kt

--- app/src/main/java/com/vitha/roomwordsample/NewWordActivity.kt
+++ app/src/main/java/com/vitha/roomwordsample/NewWordActivity.kt
@@ -4,8 +4,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class NewWordActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var editWordView: EditText
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_word)
+        editWordView = findViewById(R.id.edit_word)
+
+        val button = findViewById<Button>(R.id.button_save)
+        button.setOnClickListener {
+            val replyIntent = Intent()
+            if (TextUtils.isEmpty(editWordView.text)) {
+                setResult(Activity.RESULT_CANCELED, replyIntent)
+            } else {
+                val word = editWordView.text.toString()
+                replyIntent.putExtra(EXTRA_REPLY, word)
+                setResult(Activity.RESULT_OK, replyIntent)
+            }
+            finish()
+        }
+    }
+
+    companion object {
+        const val EXTRA_REPLY = "com.example.android.wordlistsql.REPLY"
     }
 }
