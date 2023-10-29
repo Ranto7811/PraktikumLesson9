93adc6dfa3ea35c9ad3d7974a3beda21f7f8ee70
aviliairs <avilia.indira29@gmail.com>
2023-10-29 11:39:46 +0700

    Menambahkan val applicationScope pada WordsApplication.kt

--- app/src/main/java/com/example/lesson9/WordsApplication.kt
+++ app/src/main/java/com/example/lesson9/WordsApplication.kt
@@ -3,8 +3,11 @@ package com.example.lesson9
 import android.app.Application
 
 class WordsApplication : Application() {
+    // No need to cancel this scope as it'll be torn down with the process
+    val applicationScope = CoroutineScope(SupervisorJob())
+
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
