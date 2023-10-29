62dcff1f8e52ab6bd014db106aa2eb051c8c513d
asterracea <alifwidya25@gmail.com>
2023-10-29 09:21:28 +0700

    membuat variabel allWords

--- app/src/main/java/com/example/lesson9/WordDao.kt
+++ app/src/main/java/com/example/lesson9/WordDao.kt
@@ -1,5 +1,11 @@
 package com.example.lesson9
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface WordDao {
 
--- app/src/main/java/com/example/lesson9/WordRepository.kt
+++ app/src/main/java/com/example/lesson9/WordRepository.kt
@@ -1,4 +1,14 @@
 package com.example.lesson9
 
-class WordRepository {
+import kotlinx.coroutines.flow.Flow
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class WordRepository(private val wordDao: WordDao) {
+
+    // Room executes all queries on a separate thread.
+    // Observed Flow will notify the observer when the data has changed.
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+
 }
