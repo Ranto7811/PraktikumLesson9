1ceb09c2d23479cc9df4e95aae26a48bc9914142
asterracea <alifwidya25@gmail.com>
2023-10-29 13:30:38 +0700

    menambahkan class class WordViewModelFactory

--- app/src/main/java/com/example/lesson9/WordViewModel.kt
+++ app/src/main/java/com/example/lesson9/WordViewModel.kt
@@ -1,9 +1,6 @@
 package com.example.lesson9
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import kotlinx.coroutines.launch
 
 class WordViewModel(private val repository: WordRepository) : ViewModel() {
@@ -19,5 +16,14 @@ class WordViewModel(private val repository: WordRepository) : ViewModel() {
      */
     fun insert(word: Word) = viewModelScope.launch {
         repository.insert(word)
+    }
+    class WordViewModelFactory(private val repository: WordRepository) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(WordViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return WordViewModel(repository) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
     }
 }
