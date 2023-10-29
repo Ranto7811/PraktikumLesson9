fbe19a676c69af0d48e5e4114910a2908100a6f7
asterracea <alifwidya25@gmail.com>
2023-10-29 09:50:50 +0700

    menambahkan tata letak styles.xml

--- app/src/main/java/com/example/lesson9/WordRoomDatabase.kt
+++ app/src/main/java/com/example/lesson9/WordRoomDatabase.kt
@@ -1,6 +1,9 @@
 package com.example.lesson9
 
 import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 // Annotates class to be a Room Database with a table (entity) of the Word class
 @Database(entities = arrayOf(Word::class), version = 1, exportSchema = false)
